a62e36e9bc5741568ca96f84a6d889c17e89bdb3
DJPajares <dj.pajares@gmail.com>
2023-08-11 23:16:39 +0800

    chore: use mockdata for storybook

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -1,37 +1,18 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 import Table from '../components/table/Table'; // Update the import path as needed
-import type { TableRow, TableProps } from '../types';
+import type { TableRow, TableProps, TableColumn } from '../types';
+import jsonData from '../mockData/data.json';
+import jsonColumn from '../mockData/columns.json';
 
 export default {
   title: 'Components/Table',
   component: Table
 } as Meta;
 
-const sampleData: TableRow[] = [
-  { id: 1, name: 'John Doe', age: 30, city: 'New York' },
-  { id: 2, name: 'Jane Smith', age: 25, city: 'Los Angeles' },
-  { id: 3, name: 'Mario Kart', age: 99, city: 'Marioland' }
-];
+const sampleData: TableRow[] = jsonData;
 
-const sampleColumn = [
-  {
-    key: 'id',
-    label: 'ID'
-  },
-  {
-    key: 'name',
-    label: 'Name'
-  },
-  {
-    key: 'age',
-    label: 'Age'
-  },
-  {
-    key: 'city',
-    label: 'City'
-  }
-];
+const sampleColumn: TableColumn[] = jsonColumn;
 
 const Template: Story<TableProps> = (args) => <Table {...args} />;
 
